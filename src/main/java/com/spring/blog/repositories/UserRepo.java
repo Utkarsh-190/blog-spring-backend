71b6c112f340f9cb563df9e81bbf429bd16d8631
Utkarsh-190 <utkarshtyagi62@gmail.com>
2023-09-09 23:10:08 +0530

    initialized spring project

---
+++ src/main/java/com/spring/blog/repositories/UserRepo.java
@@ -0,0 +1,11 @@
+package com.spring.blog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.blog.entities.User;
+
+public interface UserRepo extends JpaRepository<User, Integer>{
+	
+	
+
+}
---
+++ src/main/java/com/spring/blog/services/UserService.java
@@ -0,0 +1,5 @@
+package com.spring.blog.services;
+
+public class UserService {
+
+}
